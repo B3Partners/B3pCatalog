c54ce34285ca8810f841381cace4ea8bb2a782fc
Erik van de Pol <erikvandepol@b3partners.nl>
2011-02-25 16:25:36 +0000

    FGDBHelper gemaakt. Dit backend is redelijk compleet. Nu nog FGDB in filetree krijgen. Dan uitgebreid testen.

---
+++ src/java/nl/b3p/catalog/fgdb/FGDBHelper.java
@@ -0,0 +1,166 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.b3p.catalog.fgdb;
+
+import com.esri.arcgis.datasourcesGDB.FgdbFeatureClassName;
+import com.esri.arcgis.datasourcesGDB.FileGDBWorkspaceFactory;
+import com.esri.arcgis.geodatabase.FeatureClass;
+import com.esri.arcgis.geodatabase.FeatureDataset;
+import com.esri.arcgis.geodatabase.FeatureDatasetName;
+import com.esri.arcgis.geodatabase.IDataset;
+import com.esri.arcgis.geodatabase.IEnumDataset;
+import com.esri.arcgis.geodatabase.Workspace;
+import com.esri.arcgis.geodatabase.XmlPropertySet;
+import com.esri.arcgis.geodatabase.esriDatasetType;
+import java.io.File;
+import java.io.FilenameFilter;
+import nl.b3p.catalog.B3PCatalogException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ *
+ * @author Erik
+ */
+public class FGDBHelper {
+    private final static Log log = LogFactory.getLog(FGDBHelper.class);
+
+    // apart dataType doorgeven vanuit jsp. In filetree als attr zetten.
+    // kan namelijk zelfde naam hebben voor een dataset en featureclass in dezelfde gdb. pad is dan hetzelfde: bv C:\\usa.gdb\\highway
+    public static XmlPropertySet isFGDB(String filename) throws Exception {
+        File file = new File(filename);
+        if (file.exists())
+            return null;
+
+        if (isFGDBDir(file))
+            throw new UnsupportedOperationException("Metadata for an entire File GeoDB is not yet supported.");
+
+        File parent = file.getParentFile();
+        if (parent == null) {
+            return null;
+        } else if (parent.exists()) {
+            if (!parent.isDirectory())
+                return null;
+            if (isFGDBDir(parent)) {
+                // TODO geef doorgegeven featureType mee:
+                return getXmlPropertySet(parent.getAbsolutePath(), file.getName(), esriDatasetType.esriDTFeatureClass);
+            } else {
+                throw new Exception("Wrong path: " + filename + ". Looking for FGDB at: " + parent.getAbsolutePath());
+            }
+        } else {
+            // oneindige nesting lijkt niet mogelijk in ArcCatalog: 2 diep is max: Dataset met daarin iets anders.
+            File grandParent = parent.getParentFile();
+            if (grandParent == null || !grandParent.isDirectory() || !isFGDBDir(grandParent))
+                throw new Exception("Wrong path: " + filename + ". Looking for FGDB at: " + grandParent.getAbsolutePath() + ". Only max depth of 2 supported in a FGDB.");
+
+            // Dit kan als het goed is alleen maar van type esriDTFeatureDataset zijn.
+            return getXmlPropertySet(grandParent.getAbsolutePath(), parent.getName(), esriDatasetType.esriDTFeatureDataset);
+        }
+    }
+
+    private static boolean isFGDBDir(File file) {
+        if (!file.isDirectory())
+            return false;
+        String[] gdbFiles = file.list(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.equals("gdb");
+            }
+        });
+        return gdbFiles.length >= 1;
+    }
+
+    public static String getMetadata(String fileGDBPath, String dataset, int datasetType) throws Exception {
+        XmlPropertySet xmlPropertySet = getXmlPropertySet(fileGDBPath, dataset, datasetType);
+        return getMetadata(xmlPropertySet);
+    }
+
+    public static void setMetadata(String fileGDBPath, String dataset, String metadata, int datasetType) throws Exception {
+        XmlPropertySet xmlPropertySet = getXmlPropertySet(fileGDBPath, dataset, datasetType);
+        setMetadata(xmlPropertySet, metadata);
+    }
+
+    public static String getMetadata(XmlPropertySet xmlPropertySet) throws Exception {
+        return xmlPropertySet.getXml("/");
+    }
+
+    public static void setMetadata(XmlPropertySet xmlPropertySet, String metadata) throws Exception {
+        xmlPropertySet.setXml(metadata);
+    }
+
+    /*private static XmlPropertySet getFGDBXmlPropertySet(String fileGDBPath) throws Exception {
+        Workspace workspace = getFGDB(fileGDBPath);
+        WorkspaceName workspaceName = (WorkspaceName) workspace.getFullName();
+        return (XmlPropertySet) workspaceName.get.getMetadata();
+    }*/
+
+    private static XmlPropertySet getXmlPropertySet(String fileGDBPath, String dataset, int datasetType) throws Exception {
+        IDataset ds = getDataset(fileGDBPath, dataset, datasetType);
+        log.debug(ds);
+        switch(datasetType) {
+            case esriDatasetType.esriDTFeatureDataset: {
+                FeatureDataset fDataset = new FeatureDataset(ds);
+                FeatureDatasetName fDatasetName = (FeatureDatasetName)fDataset.getFullName();
+                return (XmlPropertySet)fDatasetName.getMetadata();
+            }
+            case esriDatasetType.esriDTFeatureClass: {
+                FeatureClass fClass = new FeatureClass(ds);
+                FgdbFeatureClassName fclassName = (FgdbFeatureClassName)fClass.getFullName();
+                return (XmlPropertySet)fclassName.getMetadata();
+            }
+            default:
+                throw new B3PCatalogException("DatasetType " + datasetType + " not supported in a FGDB");
+        }
+    }
+
+    private static IDataset getDataset(String fileGDBPath, String dataset, int datasetType) throws Exception {
+        IEnumDataset enumDataset = getDatasets(fileGDBPath, datasetType);
+        IDataset ds;
+        while ((ds = enumDataset.next()) != null) {
+            if (ds.getName().equals(dataset))
+                return ds;
+        }
+        throw new B3PCatalogException("Dataset " + dataset + " not found in FileGDB " + fileGDBPath);
+    }
+
+    public static void logAllDatasets(String fileGDBPath) throws Exception {
+        IEnumDataset enumDataset = getAnyDatasets(fileGDBPath);
+        log.debug("esriDatasetType.esriDTAny: " + esriDatasetType.esriDTAny);
+        log.debug("esriDatasetType.esriDTContainer: " + esriDatasetType.esriDTContainer);
+        log.debug("esriDatasetType.esriDTFeatureDataset: " + esriDatasetType.esriDTFeatureDataset);
+        log.debug("esriDatasetType.esriDTFeatureClass: " + esriDatasetType.esriDTFeatureClass);
+        log.debug("esriDatasetType.esriDTGeometricNetwork: " + esriDatasetType.esriDTGeometricNetwork);
+        IDataset ds;
+        while ((ds = enumDataset.next()) != null) {
+            log.debug(ds.getName());
+            log.debug(ds.getType());
+        }
+    }
+
+    private static IEnumDataset getDatasets(String fileGDBPath, int esriDatasetType) throws Exception {
+        Workspace workspace = getWorkspace(fileGDBPath);
+        return workspace.getDatasets(esriDatasetType);
+    }
+
+    private static IEnumDataset getFeatureClassDatasets(String fileGDBPath) throws Exception {
+        Workspace workspace = getWorkspace(fileGDBPath);
+        return workspace.getDatasets(esriDatasetType.esriDTFeatureClass);
+    }
+
+    private static IEnumDataset getFeatureDatasets(String fileGDBPath) throws Exception {
+        Workspace workspace = getWorkspace(fileGDBPath);
+        return workspace.getDatasets(esriDatasetType.esriDTFeatureDataset);
+    }
+
+    private static IEnumDataset getAnyDatasets(String fileGDBPath) throws Exception {
+        Workspace workspace = getWorkspace(fileGDBPath);
+        return workspace.getDatasets(esriDatasetType.esriDTAny);
+    }
+
+    private static Workspace getWorkspace(String fileGDBPath) throws Exception {
+        FileGDBWorkspaceFactory factory = new FileGDBWorkspaceFactory();
+        return new Workspace(factory.openFromFile(fileGDBPath, 0));
+    }
+}
--- src/java/nl/b3p/catalog/stripes/MetadataAction.java
+++ src/java/nl/b3p/catalog/stripes/MetadataAction.java
@@ -5,13 +5,6 @@
 
 package nl.b3p.catalog.stripes;
 
-import com.esri.arcgis.datasourcesGDB.FgdbFeatureClassName;
-import com.esri.arcgis.datasourcesGDB.FileGDBWorkspaceFactory;
-import com.esri.arcgis.geodatabase.FeatureClass;
-import com.esri.arcgis.geodatabase.IDataset;
-import com.esri.arcgis.geodatabase.IEnumDataset;
-import com.esri.arcgis.geodatabase.Workspace;
-import com.esri.arcgis.geodatabase.XmlPropertySet;
 import com.esri.arcgis.geodatabase.esriDatasetType;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -32,6 +25,7 @@ import net.sourceforge.stripes.validation.Validate;
 import nl.b3p.catalog.B3PCatalogException;
 import nl.b3p.catalog.HtmlErrorResolution;
 import nl.b3p.catalog.XmlResolution;
+import nl.b3p.catalog.fgdb.FGDBHelper;
 import nl.b3p.catalog.filetree.Rewrite;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
@@ -86,8 +80,10 @@ public class MetadataAction extends DefaultAction {
         File mdFile = null;
         try {
             //ArcObjects test:
+            FGDBHelper.logAllDatasets("C:\\Users\\Erik\\Documents\\ArcCatalogRoot\\data\\usa\\usa.gdb");
             log.debug("FileGDB metadata:");
-            log.debug(getFileGDBMetadata("C:\\Users\\Erik\\Documents\\ArcCatalogRoot\\data\\usa\\usa.gdb", "wind"));
+            //log.debug(FGDBHelper.getMetadata("C:\\Users\\Erik\\Documents\\ArcCatalogRoot\\data\\usa\\usa.gdb", "highway", esriDatasetType.esriDTFeatureDataset));
+            log.debug(FGDBHelper.getMetadata("C:\\Users\\Erik\\Documents\\ArcCatalogRoot\\data\\usa\\usa.gdb", "wind", esriDatasetType.esriDTFeatureClass));
 
             //if (!getContext().getRequest().isUserInRole(ROLE_VIEWER))
             //    throw new B3PCatalogException("Only viewers can view metadata files");
@@ -108,33 +104,6 @@ public class MetadataAction extends DefaultAction {
             log.error(message, e);
             return new HtmlErrorResolution(message, e);
         }
-    }
-
-    private String getFileGDBMetadata(String fileGDBPath, String layer) throws Exception {
-        XmlPropertySet xmlPropertySet = getFileGDBLayerXmlPropertySet(fileGDBPath, layer);
-        return xmlPropertySet.getXml("/");
-    }
-
-    private void setFileGDBMetadata(String fileGDBPath, String layer, String metadata) throws Exception {
-        XmlPropertySet xmlPropertySet = getFileGDBLayerXmlPropertySet(fileGDBPath, layer);
-        xmlPropertySet.setXml(metadata);
-    }
-
-    private XmlPropertySet getFileGDBLayerXmlPropertySet(String fileGDBPath, String layer) throws Exception {
-        FileGDBWorkspaceFactory factory = new FileGDBWorkspaceFactory();
-        Workspace workspace = new Workspace(factory.openFromFile(fileGDBPath, 0));
-
-        IEnumDataset enumDataset = workspace.getDatasets(esriDatasetType.esriDTFeatureClass);
-        IDataset ds;
-        while ((ds = enumDataset.next()) != null) {
-            FeatureClass fClass = new FeatureClass(ds);
-            // alias name is denk ik niet uniek. FullName gebruiken. iets regelen met display...
-            if (fClass.getAliasName().equals(layer)) {
-                FgdbFeatureClassName fclassName = (FgdbFeatureClassName) fClass.getFullName();
-                return (XmlPropertySet) fclassName.getMetadata();
-            }
-        }
-        throw new B3PCatalogException("Layer " + layer + " not found in FileGDB " + fileGDBPath);
     }
 
     public Resolution save() {
