fb8b7c0828a268e4055e4b6f95498e5b7b2c4ffe
Matthijs Laan <matthijsln@gmail.com>
2011-09-26 08:14:20 +0000

    emailadressen toegevoegd, utf8 encoding voor speciale karakters
    Former-commit-id: 17e2e24a0c5e90a445a4a415ebe23f0b516bd046

--- deploy/Harderwijk/scripts/mde/picklists/organisations.js
+++ deploy/Harderwijk/scripts/mde/picklists/organisations.js
@@ -49,9 +49,27 @@ organisations = {
         "email": "info@harderwijk.nl",
         "voice": "0341-411911",
         "contacts": {
-            "Johan Ruiter": {
-                "email": "J.Ruiter@harderwijk.nl"
-            }
+            "Edwin Gerrits":        {"email": "e.gerrits@harderwijk.nl"},
+            "Eric Klijntunte":      {"email": "e.klijntunte@harderwijk.nl"},
+            "Erwin Priester":       {"email": "e.priester@harderwijk.nl"},
+            "Gerrit Grootkarzijn":  {"email": "g.grootkarzijn@harderwijk.nl"},
+            "Gert Foppen":          {"email": "g.foppen@harderwijk.nl"},
+            "Henjacco Kassing":     {"email": "h.kassing@harderwijk.nl"},
+            "Henk Maas":            {"email": "h.maas@harderwijk.nl"},
+            "Jan Hilhorst":         {"email": "j.hilhorst@harderwijk.nl"},
+            "Jan Nijendijk":        {"email": "j.nijendijk@harderwijk.nl"},
+            "Jan Provoost":         {"email": "j.provoost@harderwijk.nl"},
+            "Jan Rozenberg":        {"email": "j.rozenberg@harderwijk.nl"},
+            "Jenny Kooij":          {"email": "j.kooij@harderwijk.nl"},
+            "Johan Ruiter":         {"email": "j.ruiter@harderwijk.nl"},
+            "Mandy Siep":           {"email": "m.siep@harderwijk.nl"},
+            "Marc Bouw":            {"email": "m.bouw@harderwijk.nl"},
+            "Nico Otten":           {"email": "n.otten@harderwijk.nl"},
+            "Pascal Lunshof":       {"email": "p.lunshof@harderwijk.nl"},
+            "Peter Mulder":         {"email": "p.mulder@harderwijk.nl"},
+            "Reinout van Drunen":   {"email": "r.vandrunen@harderwijk.nl"},
+            "Sükran Koç-Kirmiziyüz":{"email": "s.koc-kirmiziyuz@harderwijk.nl"},
+            "Sylvia van Itegem":    {"email": "s.vanitegem@harderwijk.nl"}
         }
     }
 };
