2d1af5c0debd5b1e452978a6b7188d72168d0671
Matthijs Laan <matthijsln@gmail.com>
2011-11-16 20:02:10 +0000

    organisations json juiste charset
    json validatie via hack toegevoegd (hack nodig vanwege braindead code van Erik). Hiervoor is speciale tekst in organisations.js verplicht om uit JS de JSON te halen
    
    laad standaard organisations uit mde indien config bestand niet bestaat, sla gewijzigde wel op naar geconfigureerd bestand (default staat naast config.xml in organisations.js), dus config waarde is nu optioneel
    
    
    
    Former-commit-id: 5743beb2607eb0e1e7048ce217c77d6d30093f6f

--- src/java/nl/b3p/catalog/stripes/OrganisationsAction.java
+++ src/java/nl/b3p/catalog/stripes/OrganisationsAction.java
@@ -5,6 +5,8 @@
 package nl.b3p.catalog.stripes;
 
 import java.io.File;
+import java.io.IOException;
+import javax.servlet.ServletContext;
 import net.sourceforge.stripes.action.ActionBeanContext;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.Resolution;
@@ -13,6 +15,8 @@ import nl.b3p.catalog.config.CatalogAppConfig;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  *
@@ -20,6 +24,10 @@ import org.apache.commons.logging.LogFactory;
  */
 public class OrganisationsAction extends DefaultAction {
     private final static Log log = LogFactory.getLog(OrganisationsAction.class);
+    
+    private static final String ENCODING = "UTF-8";
+    
+    private static final String DEFAULT_ORGANISATIONS_FILE = "/scripts/mde/picklists/organisations.js";
     
     @DefaultHandler
     public Resolution main() {
@@ -30,12 +38,12 @@ public class OrganisationsAction extends DefaultAction {
         // In MDE niet met <script> tag maar met XHR laden (mogelijk probleem 
         // met XHR in ArcCatalog plugin?)
         
-        return new StreamingResolution("text/javascript", organisations);
+        return new StreamingResolution("text/javascript; charset=" + ENCODING, organisations);
     }
     
     public static String getOrganisations(ActionBeanContext context) {
         try {
-            return FileUtils.readFileToString(getOrganisationsConfigFile(context), "UTF-8");
+            return FileUtils.readFileToString(getOrganisationsConfigFileOrDefault(context.getServletContext()), ENCODING);
         } catch (Exception ex) {
             log.error("Cannot read organisations config file: " + ex.getMessage());
             
@@ -44,18 +52,41 @@ public class OrganisationsAction extends DefaultAction {
     }
     
     public static void setOrganisations(ActionBeanContext context, String organisations) {
+        // XXX enorme hack
+        String search = "/* DEZE REGEL NIET WIJZIGEN */ organisations = ";
+        int i = organisations.lastIndexOf(search);
+        if(i == -1) {
+            throw new IllegalArgumentException("JavaScript syntax ongeldig: geen beginmarkering gevonden");
+        }
+               
+        String jsonPart = organisations.substring(i + search.length());
         try {
-            FileUtils.writeStringToFile(getOrganisationsConfigFile(context), organisations, "utf-8");
+            new JSONObject(jsonPart);
+        } catch(JSONException e) {
+            throw new IllegalArgumentException("JSON syntax ongeldig: " + e.getMessage());
+        }
+        try {
+            FileUtils.writeStringToFile(getOrganisationsConfigFile(), organisations,ENCODING);
         } catch (Exception ex) {
             log.error("Cannot store organisations in config file", ex);
         }
     }
     
-    protected static File getOrganisationsConfigFile(ActionBeanContext context) {
-        CatalogAppConfig cfg = CatalogAppConfig.getConfig();
+    private static File getOrganisationsConfigFile()  {
+        CatalogAppConfig cfg = CatalogAppConfig.getConfig();       
         File f = new File(cfg.getOrganizationsJsonFile());
         if(!f.isAbsolute()) {
             f = new File(cfg.getConfigFilePath(), cfg.getOrganizationsJsonFile());
+        }
+        return f;
+    }
+    
+    private static File getOrganisationsConfigFileOrDefault(ServletContext context) throws IOException {
+        File f = getOrganisationsConfigFile();
+        if(!f.exists()) {
+            String oldName = f.getCanonicalPath();
+            f = new File(context.getRealPath(DEFAULT_ORGANISATIONS_FILE));
+            log.warn(String.format("organizationsJsonFile \"%s\" bestaat niet, standaard bestand \"%s\" wordt gebruikt", oldName, f.getCanonicalPath()));
         }
         return f;
     }
